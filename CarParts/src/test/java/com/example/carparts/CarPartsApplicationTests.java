bae0735aa922c228d16b14b38d1f81a3a90a8321
jirikloudak <74323691+jirikloudak@users.noreply.github.com>
2021-11-01 16:51:19 +0100

    New project
    New project

---
+++ CarParts/src/test/java/com/example/carparts/CarPartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.carparts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarPartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
